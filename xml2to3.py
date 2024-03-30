06b5131c3a77c17d4a554a0f008bf7fd5852deb1
astyd256 <astyd637@gmail.com>
2024-03-30 19:11:37 +0700

    Lot's of fixes, added exception transform from python 2 to 3

--- xml2to3.py
+++ xml2to3.py
@@ -14,16 +14,27 @@ output_file = args.output if args.output else file_path + '.tmp'
 
 modules = {file.split('.')[0] for file in os.listdir(modules_folder) if file.endswith('.py')}
 
-pattern_import = "from\s(\w+)\simport\s([A-Za-z0-9_,\s]+)(?=\n)"
+pattern_import_from = r"(.*?from\s+)(\w+)(\s+import\s+[\w*,\s]+)"
 pattern_print = r'print\s*("([^"]*)"|\'([^\']*)\')'
+pattern_import = r"(^.*?)(import\s)([\w_]+)"
+pattern_except = r"except\s+([A-Za-z0-9_]+)\s*,\s*([a-z][A-Za-z0-9_]*)\s*:"
+except_replacement = r"except \1 as \2:"
 
-def replace_import(match):
-    module, classes = match.groups()
+
+
+def replace_import_from(match):
+    prefix, module, postfix = match.groups()
     if module in modules:
-        return f"from .{module} import {classes}"
+        return f"{prefix} .{module} {postfix}"
     else:
         return match.group(0)
 
+def replace_import(match):
+    prefix, imp, module = match.groups()
+    if module in modules:
+        return f"{prefix}from . import {module}"
+    else:
+        return match.group(0)
 
 def replace_print(match):
     if match.group(2):
@@ -31,10 +42,19 @@ def replace_print(match):
     else:
         return "print('{}')".format(match.group(3))
 
+def process_line(line):
+    if re.match(pattern_import_from, line):
+        line = re.sub(pattern_import_from, replace_import_from, line)
+    else:
+        line = re.sub(pattern_import, replace_import, line)
+    
+    line = re.sub(pattern_print, replace_print, line)
+    line = re.sub(pattern_except, except_replacement, line)
+    return line
+
 with open(file_path, 'r', encoding='utf-8') as file, open(file_path + '.tmp', 'w', encoding='utf-8') as outfile:
      for line in file:
-        line = re.sub(pattern_import, replace_import, line)
-        line = re.sub(pattern_print, replace_print, line)
+        line = process_line(line)
         outfile.write(line)
         
 print(f"File processed. Output saved to '{output_file}'.")
